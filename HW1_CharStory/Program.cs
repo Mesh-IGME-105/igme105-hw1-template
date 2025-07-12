e95eaee07071348d67851609c114b5ece8d0ace7
Erika Mesh <esmvcs@rit.edu>
2025-07-11 23:53:23 -0400

    Basic console app template & README for HW1

---
+++ HW1_CharStory/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+/***
+ * YOUR NAME
+ * 
+ * HW 1 - Character Story
+ * GitHub repo URL: 
+ * 
+ */
+namespace HW1_CharStory
+{
+    /// <summary>
+    /// Primary class for the console app. Main() will be run on program launch.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Launches the console app.
+        /// 
+        /// ALL of your source code for HW1 should be within Main 
+        /// and follow all IGME-105 course coding standards.
+        /// 
+        /// Do NOT use any additional classes, methods, or other techniques 
+        /// that have not been covered in class.
+        /// </summary>
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
